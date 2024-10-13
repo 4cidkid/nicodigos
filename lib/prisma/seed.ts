82220f76005916105a182894fcc5909552a4ca28
Nicolas Torres <nicotordev@gmail.com>
2024-10-13 11:57:25 -0300

    Refactor seed script to use faker package for generating dynamic data

--- lib/prisma/seed.ts
+++ lib/prisma/seed.ts
@@ -1,136 +1,62 @@
 import { PrismaClient } from "@prisma/client";
 import { consola } from "consola";
+import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 async function main() {
   const categories = await prisma.category.createManyAndReturn({
-    data: [
-      {
-        name: "Windows keys",
-        slug: "windows",
-        description: "Windows keys",
-        image: "https://via.placeholder.com/150",
-        icon: "https://via.placeholder.com/50",
-      },
-      {
-        name: "Office keys",
-        slug: "office",
-        description: "Office keys",
-        image: "https://via.placeholder.com/150",
-        icon: "https://via.placeholder.com/50",
-      },
-      {
-        name: "Antivirus keys",
-        slug: "antivirus",
-        description: "Antivirus keys",
-        image: "https://via.placeholder.com/150",
-        icon: "https://via.placeholder.com/50",
-      },
-      {
-        name: "Games keys",
-        slug: "games",
-        description: "Games keys",
-        image: "https://via.placeholder.com/150",
-        icon: "https://via.placeholder.com/50",
-      },
-    ],
+    data: Array.from({ length: 10 }).map(() => ({
+      name: faker.commerce.department(),
+      slug: faker.lorem.slug(),
+      description: faker.commerce.productDescription(),
+      image: faker.image.urlPlaceholder({ width: 150, height: 150 }),
+      icon: faker.image.urlPlaceholder({ width: 50, height: 50 }),
+    })),
   });
 
   const products = await prisma.product.createManyAndReturn({
-    data: [
-      {
-        name: "Windows 10 Pro",
-        slug: "windows-10-pro",
-        description: "Windows 10 Pro",
-        image: "https://via.placeholder.com/150",
-        price: 100,
-        categoryId: categories[0].id,
-      },
-      {
-        name: "Office 2019",
-        slug: "office-2019",
-        description: "Office 2019",
-        image: "https://via.placeholder.com/150",
-        price: 50,
-        categoryId: categories[1].id,
-      },
-      {
-        name: "Kaspersky Total Security",
-        slug: "kaspersky-total-security",
-        description: "Kaspersky Total Security",
-        image: "https://via.placeholder.com/150",
-        price: 30,
-        categoryId: categories[2].id,
-      },
-      {
-        name: "FIFA 21",
-        slug: "fifa-21",
-        description: "FIFA 21",
-        image: "https://via.placeholder.com/150",
-        price: 60,
-        categoryId: categories[3].id,
-      },
-    ],
+    data: Array.from({ length: 20 }).map(() => ({
+      name: faker.commerce.productName(),
+      slug: faker.lorem.slug(),
+      description: faker.commerce.productDescription(),
+      image: faker.image.urlPlaceholder({ width: 300, height: 300 }),
+      price: Number(
+        faker.commerce.price({
+          min: 10,
+          max: 100,
+          dec: 2,
+        })
+      ),
+      categoryId: faker.helpers.arrayElement(categories).id,
+    })),
   });
 
   const attributes = await prisma.attribute.createManyAndReturn({
-    data: [
-      {
-        name: "License type",
-        icon: "https://via.placeholder.com/50",
-        description: "License type",
-      },
-      {
-        name: "Delivery type",
-        icon: "https://via.placeholder.com/50",
-        description: "Delivery type",
-      },
-    ],
+    data: Array.from({ length: 5 }).map(() => ({
+      name: faker.commerce.productMaterial(),
+      icon: faker.image.urlPlaceholder({ width: 50, height: 50 }),
+      description: faker.lorem.sentence(),
+    })),
   });
 
   await prisma.productAttribute.createMany({
-    data: [
-      {
-        productId: products[0].id,
-        attributeId: attributes[0].id,
-        value: "OEM",
-      },
-      {
-        productId: products[0].id,
-        attributeId: attributes[1].id,
-        value: "Instant",
-      },
-      {
-        productId: products[1].id,
-        attributeId: attributes[0].id,
-        value: "Retail",
-      },
-      {
-        productId: products[1].id,
-        attributeId: attributes[1].id,
-        value: "Instant",
-      },
-      {
-        productId: products[2].id,
-        attributeId: attributes[0].id,
-        value: "Retail",
-      },
-      {
-        productId: products[2].id,
-        attributeId: attributes[1].id,
-        value: "Instant",
-      },
-      {
-        productId: products[3].id,
-        attributeId: attributes[0].id,
-        value: "Retail",
-      },
-      {
-        productId: products[3].id,
-        attributeId: attributes[1].id,
-        value: "Instant",
-      },
-    ],
+    data: products.flatMap((product) => {
+      return attributes.map((attribute) => ({
+        productId: product.id,
+        attributeId: attribute.id,
+        value: faker.commerce.productAdjective(),
+      }));
+    }),
+  });
+
+  await prisma.subcategory.createMany({
+    data: Array.from({ length: 20 }).map(() => ({
+      name: faker.commerce.productName(),
+      slug: faker.lorem.slug(),
+      icon: faker.image.urlPlaceholder({ width: 50, height: 50 }),
+      description: faker.commerce.productDescription(),
+      categoryId: faker.helpers.arrayElement(categories).id,
+    })),
   });
 }
 
