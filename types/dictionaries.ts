31622ab67a4776e20a05e28e41a04e398ee2c7c1
Nicolas Torres <nicotordev@gmail.com>
2024-10-13 08:43:21 -0300

    Add types for dictionaries

---
+++ types/dictionaries.ts
@@ -0,0 +1,10 @@
+import headerDictionary from "@/dictionaries/layout/header/en.json";
+import homeDictionary from "@/dictionaries/pages/home/en.json";
+type HeaderDictionary = typeof headerDictionary;
+type HomeDictionary = typeof homeDictionary;    
+
+
+export type {
+    HeaderDictionary,
+    HomeDictionary
+}
