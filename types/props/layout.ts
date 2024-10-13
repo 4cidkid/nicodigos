638bfaef75bdae2eb26a5a1c863eb7d32fc3c3f7
Nicolas Torres <nicotordev@gmail.com>
2024-10-13 11:57:31 -0300

    Refactor layout.ts to include AllCategoriesDrawerProps

--- types/props/layout.ts
+++ types/props/layout.ts
@@ -1,10 +1,28 @@
-import { Category } from "@prisma/client";
 import { Session } from "next-auth";
 import { HeaderDictionary } from "../dictionaries";
 type HeaderProps = {
-  categories: Category[];
+  categories: {
+    name: string;
+    slug: string;
+    icon: string;
+    subCategories: {
+      name: string;
+      slug: string;
+    }[];
+  }[];
   session: Session | null;
   headerDictionary: HeaderDictionary;
 };
+type AllCategoriesDrawerProps = {
+  categories: {
+    name: string;
+    slug: string;
+    icon: string;
+    subCategories: {
+      name: string;
+      slug: string;
+    }[];
+  }[];
+};
 
-export type { HeaderProps };
+export type { HeaderProps, AllCategoriesDrawerProps };
