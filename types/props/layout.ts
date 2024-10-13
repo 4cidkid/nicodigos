b8ff4bcda4a3b0e9c73153fee0fb7ba7e9be12a4
Nicolas Torres <nicotordev@gmail.com>
2024-10-13 08:43:33 -0300

    Add header props

--- types/props/layout.ts
+++ types/props/layout.ts
@@ -1,11 +1,10 @@
-import { Category } from "@prisma/client"
-
+import { Category } from "@prisma/client";
+import { Session } from "next-auth";
+import { HeaderDictionary } from "../dictionaries";
 type HeaderProps = {
-    categories: Category[];
-}
+  categories: Category[];
+  session: Session | null;
+  headerDictionary: HeaderDictionary;
+};
 
-
-
-export type {
-    HeaderProps
-}
+export type { HeaderProps };
