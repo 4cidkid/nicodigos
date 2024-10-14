8e93aea10a173a4ac358e65d1dbd59f8a7fea6b2
Nicolas Torres <nicotordev@gmail.com>
2024-10-13 23:38:46 -0300

    Refactor AllCategoriesDrawer component to update category links styling and handle category selection

--- components/Layout/AllCategoriesDrawer.tsx
+++ components/Layout/AllCategoriesDrawer.tsx
@@ -1,17 +1,23 @@
 "use client";
-import Image from 'next/image';
-import { AllCategoriesDrawerProps } from '@/types/props/layout';
-import { Drawer, NavbarLink, Sidebar } from 'flowbite-react';
-import { FaBars } from 'react-icons/fa';
-import { useState } from 'react';
+import clsx from "clsx";
+import Image from "next/image";
+import { AllCategoriesDrawerProps } from "@/types/props/layout";
+import { Drawer, NavbarLink, Sidebar } from "flowbite-react";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 export default function AllCategoriesDrawer({
   categories,
 }: AllCategoriesDrawerProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [isAllCategoriesActive, setIsAllCategoriesActive] = useState(true);
 
   const handleClose = () => setIsOpen(false);
   const handleOpen = () => setIsOpen(true);
+
+  const handleAllCategories = () => setIsAllCategoriesActive(true);
+  const handleCategory = () => setIsAllCategoriesActive(false);
+
   return (
     <>
       <NavbarLink
@@ -26,7 +32,7 @@ export default function AllCategoriesDrawer({
         }}
       >
         <div className="flex items-center gap-2 rounded-full">
-          <span className="inline-block bg-main-500 px-3 py-2 rounded-tl-full rounded-bl-full">
+          <span className="inline-block bg-main-600 px-3 py-2 rounded-tl-full rounded-bl-full">
             <FaBars className="text-base text-white" />
           </span>
           <p className="text-sm font-semibold">All Categories</p>
@@ -42,37 +48,57 @@ export default function AllCategoriesDrawer({
         }}
       >
         <Drawer.Items>
-          <button
-            className="flex items-center gap-2 rounded-full bg-main-500 w-full"
-            onClick={handleClose}
-          >
-            <span className="inline-block px-3 py-2">
-              <FaBars className="text-base text-white" />
-            </span>
-            <p className="text-sm font-semibold text-white">All Categories</p>
-          </button>
           <Sidebar
             aria-label="Sidebar with multi-level dropdown example"
-            className="[&>div]:bg-transparent [&>div]:p-0"
+            className="[&>div]:bg-transparent [&>div]:p-0 w-full"
           >
             <div className="flex h-full flex-col justify-between py-2">
               <div>
                 <Sidebar.Items>
                   <Sidebar.ItemGroup>
-                    {categories.map((category) => (
+                    <div className="contents">
                       <Sidebar.Item
-                        key={category.slug}
                         icon={() => (
-                          <Image
-                            src={category.icon}
-                            alt={category.name}
-                            width={24}
-                            height={24}
+                          <FaBars
+                            className={clsx("text-base", {
+                              "text-white": isAllCategoriesActive,
+                              "text-gray-700": !isAllCategoriesActive,
+                            })}
                           />
                         )}
+                        className={clsx(
+                          "pl-2.5 font-semibold text-sm hover:text-white rounded-full transition-colors cursor-pointer hover:bg-main-600",
+                          {
+                            "bg-main-600 text-white": isAllCategoriesActive,
+                            "bg-transparent text-gray-700":
+                              !isAllCategoriesActive,
+                          }
+                        )}
                       >
-                        {category.name}
+                        All Categories
                       </Sidebar.Item>
+                    </div>
+                    {categories.map((category) => (
+                      <div
+                        className="contents"
+                        key={category.slug}
+                        onMouseEnter={handleCategory}
+                        onMouseLeave={handleAllCategories}
+                      >
+                        <Sidebar.Item
+                          icon={() => (
+                            <Image
+                              src={category.icon}
+                              alt={category.name}
+                              width={16}
+                              height={16}
+                            />
+                          )}
+                          className="pl-2.5 font-semibold text-sm text-gray-700 hover:bg-main-600 hover:text-white rounded-full transition-colors"
+                        >
+                          {category.name}
+                        </Sidebar.Item>
+                      </div>
                     ))}
                   </Sidebar.ItemGroup>
                 </Sidebar.Items>
