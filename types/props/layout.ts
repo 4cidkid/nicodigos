9c45db8d36086d8e8fe5b7339f8440b31c281292
NicoTorDev <115031601+4cidkid@users.noreply.github.com>
2024-10-26 18:45:40 -0300

    Refactor layout.tsx to use updated types for categories and subcategories

--- types/props/layout.ts
+++ types/props/layout.ts
@@ -1,28 +1,17 @@
 import { Session } from "next-auth";
 import { HeaderDictionary } from "../dictionaries";
+import { Category, Subcategory } from "@prisma/client";
+
+type HeaderCategory = Partial<Category> & Pick<Category, 'name' | 'slug' | 'icon'> & {
+  subCategories: (Partial<Subcategory> & Pick<Subcategory, 'name' | 'slug' | 'image'>)[]
+}
 type HeaderProps = {
-  categories: {
-    name: string;
-    slug: string;
-    icon: string;
-    subCategories: {
-      name: string;
-      slug: string;
-    }[];
-  }[];
+  categories: HeaderCategory[];
   session: Session | null;
   headerDictionary: HeaderDictionary;
 };
 type AllCategoriesDrawerProps = {
-  categories: {
-    name: string;
-    slug: string;
-    icon: string;
-    subCategories: {
-      name: string;
-      slug: string;
-    }[];
-  }[];
+  categories: HeaderCategory[];
 };
 
 export type { HeaderProps, AllCategoriesDrawerProps };
